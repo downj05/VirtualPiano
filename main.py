e7c19c4ff39952d45c54bddf5ab589fcd45784fd
downj05 <91046524+downj05@users.noreply.github.com>
2023-09-24 06:01:26 +1300

    fix

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import os
 import configparser
 
 # Modify path to load fluid synth
-app_path = os.getcwd()+r'/fluidsynth'
+app_path = os.getcwd()+r'\fluidsynth'
 print(app_path)
 if os.environ["PATH"][-1] == '.':
 	os.environ["PATH"] = os.environ["PATH"][:-1]
-os.environ["PATH"] += app_path
+os.environ["PATH"] += ';'+app_path
 print(os.environ["PATH"])
 import vp
 
